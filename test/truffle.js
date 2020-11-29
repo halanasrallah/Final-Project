5adbf4a3f4744efb1ef3681939a3b431dcfe7897
halanasrallah <halanasrallah77@users.noreply.github.com>
2020-11-29 23:36:47 +0200

    main

--- test/truffle.js
+++ test/truffle.js
@@ -1,31 +1,19 @@
-require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider');
-require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider');
+var HDWalletProvider = require('truffle-hdwallet-provider');
+const MNEMONIC = 'close priority impact punch tone strong attend input ribbon clay cheese record';
+const infuraKey = 'bd6b0c6836474fdb83893cccfd522bea';
 module.exports = {
-    networks: {
-        ropsten: {
-            provider: function() {
-                return new HDWalletProvider (
-                    process.env.MEMONIC,
-                    'https://ropsten.infura.io/${process.env.INFURA_API_KEY}'
-                )
-            },
-            gasPrice: 250000000,
-            network_id: 3
-        },
-              development: {
-                  host: "127.0.0.1",
-                  port: 8545,
-                  network_id: "*" // Match any network id
-                },
-
-                
-              
-        },
-    
-    solc: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-    } } }
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/bd6b0c6836474fdb83893cccfd522bea")
+      },
+      network_id: 3,
+      gasPrice:4000000,   
+    },
+  },
+}
